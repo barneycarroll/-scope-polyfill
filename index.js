13d9acc019b00ec42cd32c91d5511a39d45624b0
Barney <barney.carroll@gmail.com>
2024-07-19 13:31:33 +0100

    Stylistic edit

--- index.js
+++ index.js
@@ -15,44 +15,57 @@ const getUniqueSelector = (/** @type {Element} */ element) => {
 		element = parent
 	}
 
-	return ':root' + selector
+	return `:root${ selector }`
 }
 
-let supportMap = new WeakMap()
+const supportMap = new WeakMap()
 
-let polyfillScope = (document) => {
-	for (let sheet of document.styleSheets) {
+function polyfillScope(){
+	for (const sheet of document.styleSheets)
 		for (let index = 0; index < sheet.cssRules.length; ++index) {
-			let rule = sheet.cssRules[index]
-			if (rule.type === 12) {
-				let { conditionText } = rule
-				let match = conditionText.match(/^\(polyfill @scope \((.+) to\((.+)\)\)\)$/)
-				if (match) {
-					if (supportMap.has(rule)) continue
-					let [ , from, to ] = match
-					let mediaRule = sheet.cssRules[sheet.insertRule(`@media ${`@scope ${from} to(${to})`.replace(/[^\w]/g, '\\$&')},all` + rule.cssText.slice('@supports '.length + rule.conditionText.length), index++)]
-					supportMap.set(rule, mediaRule)
-					supportMap.set(mediaRule, rule)
-					let fromElements = Array.from(document.querySelectorAll(from))
-					for (let innerRule of mediaRule.cssRules) {
-						if (innerRule.type === 1) {
-							let narrElements = fromElements.map(
-								from => Array.from(
-									from.querySelectorAll(':is(' + innerRule.selectorText + '):not(:scope :is(' + to + ') *)')
-								)
-							).flat()
-							let selectors = narrElements.map(
-								element => getUniqueSelector(element)
-							)
-							innerRule.selectorText += ':where(' + selectors + ')'
-						}
-					}
-				}
-			}
+			const rule = sheet.cssRules[index]
+
+			if (rule.type !== 12)
+        continue
+
+      const match = conditionText.match(/^\(polyfill @scope \((.+) to\((.+)\)\)\)$/)
+
+      if (!match || supportMap.has(rule))
+        continue
+
+      const [ , from, to ] = match
+      const mediaRule = sheet.cssRules[
+        sheet.insertRule(
+          `@media ${
+            `@scope ${from} to(${to})`.replace(/[^\w]/g, '\\$&')
+          },all${
+            rule.cssText.slice('@supports '.length + rule.conditionText.length)
+          }`,
+
+          index++
+        )
+      ]
+
+      supportMap.set(rule, mediaRule)
+      supportMap.set(mediaRule, rule)
+
+      const fromElements = Array.from(document.querySelectorAll(from))
+
+      for (const innerRule of mediaRule.cssRules) {
+        if (innerRule.type !== 1)
+          return
+
+        const narrElements = fromElements.flatMap(
+          from => Array.from(
+            from.querySelectorAll(`:is(${ innerRule.selectorText }):not(:scope :is(${ to }) *)`)
+          )
+        )
+
+        innerRule.selectorText += `:where(${ narrElements.map(getUniqueSelector) })`
+      }
 		}
-	}
 }
 
-let observer = new MutationObserver(() => polyfillScope(document));
-observer.observe(document.documentElement, { childList: true, subtree: true });
-polyfillScope(document);
+new MutationObserver(polyfillScope).observe(document.documentElement, { childList: true, subtree: true })
+
+polyfillScope()
